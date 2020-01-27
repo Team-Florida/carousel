18ede2d0e671565c73291ca0f27a90acbac77a6c
Pankaj <pgrover2079@gmail.com>
2020-01-27 14:37:02 -0800

    Structure for Modal setup using styled-components complete

---
+++ client/src/components/ModalIndex.jsx
@@ -0,0 +1,7 @@
+let ModalIndex = {};
+
+ModalIndex['ModalShare'] = require('./ModalShare.jsx').default;
+ModalIndex['ModalSave'] = require('./ModalSave.jsx').default;
+ModalIndex['ModalViewPhotos'] = require('./ModalViewPhotos.jsx').default;
+
+export default ModalIndex;
---
+++ client/src/components/ModalViewPhotos.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+// import modalCSS from '../../dist/modalStyles.module.css';
+
+import styled from 'styled-components';
+
+const ShareModalBox = styled.div`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  background-color: #ffffff;
+  padding: 10px 0px;
+  border-radius: 10px;
+`;
+
+const Heading = styled.div`
+  font-weight: bold;
+  display: inline-block;
+  margin-top: 5px;
+`;
+
+const CrossButton = styled.div`
+  float: left;
+  margin-left: 20px;
+  text-align: left;
+  font-size: 1.4em;
+  font-weight: bold;
+`;
+
+const ModalShare = props => {
+  return (
+    <ShareModalBox>
+
+      <CrossButton onClick={props.closeHandle}>X</CrossButton>
+      <Heading>Sign up to book</Heading>
+      <hr />
+
+    </ShareModalBox>
+  );
+}
+
+export default ModalShare;
