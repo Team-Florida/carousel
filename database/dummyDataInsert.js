f5323cfb874a8c42666692e9db1271b1ff73c734
Pankaj <pgrover2079@gmail.com>
2020-01-29 21:20:29 -0800

    Connected client to database

--- database/dummyDataInsert.js
+++ database/dummyDataInsert.js
@@ -3,7 +3,16 @@ const makeFakeData = require('./fakeData.js');
 
 
 
+
 // Initial Dummy Data
-CarouseModel.insertMany(makeFakeData())
+// CarouseModel.insertMany(makeFakeData())
+//   .then(response => console.log(`${response.length} records inserted`))
+//   .catch(error => console.log(error));
+
+
+// MyData 
+const myData = require('./myData.js');
+
+CarouseModel.insertMany(myData)
   .then(response => console.log(`${response.length} records inserted`))
   .catch(error => console.log(error));
